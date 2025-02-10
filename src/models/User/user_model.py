fc983e000b25bd3b45b6075a9315b8480d98e75a
RubenPerezCastrillo <ruben.perez.castrillo@gmail.com>
2025-02-10 20:48:34 +0000

    routes and usermodel change

--- src/models/User/user_model.py
+++ src/models/User/user_model.py
@@ -7,9 +7,9 @@ class Usuario(db.Model):
     __tablename__ = 'usuarios' 
 
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50))
-    apellidos = db.Column(db.String(50))
-    nombre_usuario = db.Column(db.String(50), unique=True)
+    nombre = db.Column(db.String(50), nullable=True)
+    apellidos = db.Column(db.String(50), nullable=True)
+    nombre_usuario = db.Column(db.String(50), unique=True, nullable=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     fecha_registro = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
